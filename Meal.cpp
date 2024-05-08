033e80550775d4367edbc36fcbd39e964e416867
margotpalin <169114984+margotpalin@users.noreply.github.com>
2024-05-08 17:39:23 +1000

    Update Meal.cpp

--- Meal.cpp
+++ Meal.cpp
@@ -3,6 +3,7 @@ Meal::Meal(std::string _id, std::string _name, std::string _description, double 
         : id(_id), name(std::move(_name)), description(std::move(_description)), price(_price) {}
 
 void Meal::display() const {
-    std::cout << "ID: " << id << ", Name: " << name << ", Price: $" << std::setprecision(2) << std::fixed << price << std::endl;
-    std::cout << "Description: " << description << std::endl;
-}
+    std::cout << std::left << std::setw(10) << id
+                  << std::setw(20) << name
+                  << "$" << std::setw(8) << std::fixed << std::setprecision(2) << price << std::endl;
+    }
