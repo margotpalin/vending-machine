a64b3714209dd69b40fb3be5e47e5b8bbe0b4203
vroksss <florentbrethon@gmail.com>
2024-05-15 04:05:29 +0200

    Delete Meal.cpp

--- Meal.cpp
+++
@@ -1,9 +0,0 @@
-#include "Meal.h"
-Meal::Meal(std::string _id, std::string _name, std::string _description, double _price)
-        : id(_id), name(std::move(_name)), description(std::move(_description)), price(_price) {}
-
-void Meal::display() const {
-    std::cout << std::left << std::setw(10) << id
-                  << std::setw(20) << name
-                  << "$" << std::setw(8) << std::fixed << std::setprecision(2) << price << std::endl;
-    }
