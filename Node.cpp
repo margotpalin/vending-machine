3d4ebbf1429680475a9becbb55447963499c97f3
margotpalin <169114984+margotpalin@users.noreply.github.com>
2024-05-15 16:34:49 +1000

    Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,8 +1,4 @@
 #include "Node.h"
 
-Node::Node(){
-    // TODO
-};
-Node::~Node(){
-    // TODO
-};
+Node::Node(Meal* _data, Node* _next) : data(_data), next(_next) {}
+Node::~Node(){ delete data; } 
